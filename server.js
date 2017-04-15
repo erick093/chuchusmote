1d27ca444ad81fc1d26c2f1bb894e0ab6b0941b9
Erick Escobar <erick_093@hotmail.com>
2017-04-15 10:29:39 -0400

    Added morgan logging

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
-
+var morgan=require('morgan');
 var app = express();
+//Middleware
+app.use(morgan('dev'));
 
 app.get('/', function (req,res){
   var name = "Batman";
-  res.json("My name is" + name);
+  res.json("My name is " + name);
 });
 
 app.listen(3000, function(err){
