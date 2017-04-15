2cf96fcb1413462c0faca7459ef3f604d54833fa
Erick Escobar <erick_093@hotmail.com>
2017-04-15 10:13:45 -0400

    testing get response

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
+
 var app = express();
+
+app.get('/', function (req,res){
+  var name = "Batman";
+  res.json("My name is" + name);
+});
 
 app.listen(3000, function(err){
   if(err) throw err;
