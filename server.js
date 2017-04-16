4cb8e81978836c40ef57f4b5b7f19b69a1dde8e2
Erick Escobar <erick_093@hotmail.com>
2017-04-17 00:34:56 +0200

    Added several testing things

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-var express = require('express');
+const express = require('express');
+const hbs = require('hbs');
 var morgan=require('morgan');
 var app = express();
 //Middleware
 app.use(morgan('dev'));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.get('/',(req,res) => {
+  // res.send('<h1>Hello Chuchusmoteaaa</h1>');
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    CurrentYear: new Date().getFullYear(),
+    welcomeMessage: 'Fuck your LIFE!!'
 
-app.get('/', function (req,res){
-  var name = "Batman";
-  res.json("My name is " + name);
+  });
+});
+app.get('/about', (req,res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    CurrentYear: new Date().getFullYear()
+  });
 });
 
 app.listen(3000, function(err){
   if(err) throw err;
   console.log("Server is Runnnig on port 3000");
+
+
 });
