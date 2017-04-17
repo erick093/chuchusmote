f2fee0a7b25107d2155a1ae30e711fe3187581c1
Erick Escobar <erick_093@hotmail.com>
2017-04-17 02:39:40 +0200

    added MQTT support

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 var morgan=require('morgan');
 var app = express();
+var mqtt =require('mqtt');
+var client = mqtt.connect('mqtt://10.0.0.30:8883');
 //Middleware
 app.use(morgan('dev'));
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+//Logs the date of req and res and appends it to a server.log file
+app.use((req,res, next) =>{
+  var now = new Date().toString();
+  var log = `${now}:${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log + '\n');
+  next();
+});
+
+hbs.registerHelper('GetCurrentYear', () =>{
+  return new Date().getFullYear()
+});
+
 app.get('/',(req,res) => {
-  // res.send('<h1>Hello Chuchusmoteaaa</h1>');
   res.render('home.hbs',{
     pageTitle: 'Home Page',
-    CurrentYear: new Date().getFullYear(),
     welcomeMessage: 'Fuck your LIFE!!'
 
   });
 });
+
 app.get('/about', (req,res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    CurrentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
@@ -27,4 +42,15 @@ app.listen(3000, function(err){
   console.log("Server is Runnnig on port 3000");
 
 
+});
+
+
+//MQTT Handling
+client.on('connect', function () {
+  client.subscribe('hello');
+});
+client.on('message', function (topic, message) {
+  // message is Buffer 
+  console.log(message.toString());
+  client.end();
 });
